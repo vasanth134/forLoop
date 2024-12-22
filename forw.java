cc5a7d305e83285beb86ae12bb9a5cb3ee473db1
vasanth134 <crazyvasanth004@gmail.com>
2024-12-22 18:33:23 +0530

    Add forw.java to read user input until a value of 10 or greater is entered

---
+++ forw.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class forw {
+
+    public static void main(String args[]) {
+
+        Scanner scan = new Scanner(System.in);
+        int input;  
+        do {
+            input = scan.nextInt();
+
+        } while (input < 10);
+    }
+}
